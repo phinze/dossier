eb9c222a69547b19c7a237b525d46286d179416a
Paul Hinze <paulh@instructure.com>
2013-05-16 13:42:29 -0500

    bootstrap and index page

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ group :assets do
   # gem 'therubyracer', platforms: :ruby
 
   gem 'uglifier', '>= 1.0.3'
+
+  gem 'bootstrap-sass', '~> 2.3.1.0'
 end
 
 gem 'jquery-rails'
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,14 @@ module Dossier
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    #
+    # > Due to a change in Rails that prevents images from being compiled in
+    # > vendor and lib, you'll need to add the following line to your
+    # > application.rb:
+    #
+    # https://github.com/thomas-mcdonald/bootstrap-sass
+    #
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
